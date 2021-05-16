2ffc2c3f0ec478077ab7f22e38dfdd5128910e67
beyondreach <1123016988@qq.com>
2021-05-16 22:18:24 +0800

    第二周提交

---
+++ 第二周/HttpClient01.java
@@ -0,0 +1,40 @@
+package java0.nio01.httpclient;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class HttpClient01 {
+    public final static void main(String[] args) throws Exception {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            HttpGet httpget = new HttpGet("http://localhost:8801");
+
+            System.out.println("Executing request " + httpget.getRequestLine());
+
+            // Create a custom response handler
+            ResponseHandler<String> responseHandler = response -> {
+                int status = response.getStatusLine().getStatusCode();
+                if (status >= 200 && status < 300) {
+                    HttpEntity entity = response.getEntity();
+                    return entity != null ? EntityUtils.toString(entity) : null;
+                } else {
+                    throw new ClientProtocolException("Unexpected response status: " + status);
+                }
+            };
+            String responseBody = httpclient.execute(httpget, responseHandler);
+
+            System.out.println("----------------------------------------");
+            System.out.println(responseBody);
+        } finally {
+            httpclient.close();
+        }
+    }
+}
---
+++ 第二周/对于不同GC的总结.md
@@ -0,0 +1,35 @@
+# 对于不同GC的总结、
+
+## 串行GC
+
+> 启动参数： -XX:+PrintGCDetails -Xmx500m -Xms500m -XX:+UseSerialGC
+
+![img_3.png](img_3.png)
+
+1.使用 GCLogAnalysis.java 自己演练一遍串行 / 并行 /CMS/G1 的案例。
+
+## 并行GC
+
+> 启动参数： -XX:+PrintGCDetails -Xmx500m -Xms500m -XX:+UseParallelGC
+
+![img_4.png](img_4.png)
+
+## CMSGC
+
+> 启动参数：-XX:+PrintGCDetails -Xmx500m -Xms500m -XX:+UseConcMarkSweepGC
+
+![img_1.png](img_1.png)
+
+> 将启动参数：-XX:+PrintGCDetails 换成 -XX:+PrintGC， GC日志中只展示了CMS GC的初始化标记阶段和最终标记阶段，并发清理和并发重置阶段以full GC的形式展示
+
+![img_2.png](img_2.png)
+
+## G1GC
+
+> 启动参数：-XX:+PrintGC -Xmx500m -Xms500m -XX:+UseG1GC -XX:+PrintGCDateStamps
+
+![img_5.png](img_5.png)
+
+## 压测工具 SuperBenchmarker（sb） 的使用
+
+![img_6.png](img_6.png)
